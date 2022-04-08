c2ec559ba7edc07c9794238f581870acbf94a60e
Tian Yao <yao.tian@avient.com>
2022-04-08 16:32:53 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/TYHXX/go-miniBookingSystem/pkg/config"
+	handerls "github.com/TYHXX/go-miniBookingSystem/pkg/handlers"
+	"github.com/TYHXX/go-miniBookingSystem/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("can't create template cache")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handerls.NewRepo(&app)
+	handerls.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	// http.HandleFunc("/", handerls.Repo.Home)
+	// http.HandleFunc("/about", handerls.Repo.About)
+	// http.HandleFunc("/divide", Divide)
+	fmt.Println(fmt.Sprintf("Starting appliaction on port %s", portNumber))
+
+	// http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal((err))
+}
+
+func addValue(x, y int) int {
+	return x + y
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	var x float32 = 100.0
+	var y float32 = 0.0
+
+	f, err := devideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
+
+}
+
+func devideValues(x, y float32) (float32, error) {
+	if y == 0 {
+		err := errors.New("cant devide by zero")
+		return 0, err
+	}
+	result := x / y
+
+	return result, nil
+}
---
+++ routes.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/TYHXX/go-miniBookingSystem/pkg/config"
+	handerls "github.com/TYHXX/go-miniBookingSystem/pkg/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(handerls.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handerls.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handerls.Repo.Home)
+	mux.Get("/about", handerls.Repo.About)
+
+	return mux
+
+}
