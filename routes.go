ac30122db3c1dfc1e9fdf87bd8632c7593b8ed55
Tian Yao <yao.tian@avient.com>
2022-04-13 23:10:07 +0800

    add the html templats and handlers

--- routes.go
+++ routes.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/TYHXX/go-miniBookingSystem/pkg/config"
-	handerls "github.com/TYHXX/go-miniBookingSystem/pkg/handlers"
+	handlers "github.com/TYHXX/go-miniBookingSystem/pkg/handlers"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -23,8 +23,14 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
-	mux.Get("/", handerls.Repo.Home)
-	mux.Get("/about", handerls.Repo.About)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
