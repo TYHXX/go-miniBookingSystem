b3ec50e7d47d12405f4ecf0a1e0ff686e9d25f5d
Tian Yao <yao.tian@avient.com>
2022-04-19 17:21:18 +0800

    server side validation part 2

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -19,10 +19,16 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Valid returns true if there are no errors, otherwise false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
+		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	}
 	return true
