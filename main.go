4a50315e87b5638d283bd754be5834fa70cb9d1d
Tian Yao <yao.tian@avient.com>
2022-04-22 17:25:35 +0800

    setup the test for the main package

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"encoding/gob"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/TYHXX/go-miniBookingSystem/internal/config"
-	handerls "github.com/TYHXX/go-miniBookingSystem/internal/handlers"
+	handlers "github.com/TYHXX/go-miniBookingSystem/internal/handlers"
 	"github.com/TYHXX/go-miniBookingSystem/internal/models"
 	"github.com/TYHXX/go-miniBookingSystem/internal/render"
 
@@ -21,14 +20,34 @@ const portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
 
+// main is the main function
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
 	// change this to true when in production
 	app.InProduction = false
 
+	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -39,56 +58,16 @@ func main() {
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
-		log.Fatal("can't create template cache")
+		log.Fatal("cannot create template cache")
+		return err
 	}
+
 	app.TemplateCache = tc
 	app.UseCache = false
 
-	repo := handerls.NewRepo(&app)
-	handerls.NewHandlers(repo)
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
-
-	// http.HandleFunc("/", handerls.Repo.Home)
-	// http.HandleFunc("/about", handerls.Repo.About)
-	// http.HandleFunc("/divide", Divide)
-	fmt.Println(fmt.Sprintf("Starting appliaction on port %s", portNumber))
-
-	// http.ListenAndServe(portNumber, nil)
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	log.Fatal((err))
-}
-
-func addValue(x, y int) int {
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	var x float32 = 100.0
-	var y float32 = 0.0
-
-	f, err := devideValues(x, y)
-	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-		return
-	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
-
-}
-
-func devideValues(x, y float32) (float32, error) {
-	if y == 0 {
-		err := errors.New("cant devide by zero")
-		return 0, err
-	}
-	result := x / y
-
-	return result, nil
+	return nil
 }
---
+++ routes_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/TYHXX/go-miniBookingSystem/internal/config"
+	"github.com/go-chi/chi/v5"
+
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+	var app config.AppConfig
+
+	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case *chi.Mux:
+		// do nothing
+	default:
+		t.Error(fmt.Sprintf("type is not *chi.Mux, but is %T", v))
+	}
+}
